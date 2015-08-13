1bcebf546b322d29b35f6172c2d0deea13203ca1
Eleanor Paxman <epaxman@osx1010eployed2.qagoma.local>
2015-08-13 11:10:25 +1000

    First pages commit

--- voyage.js
+++
@@ -1,9 +0,0 @@
-Crafty.scene("Voyage", function() {
-	
-	//black background with some loading text
-    Crafty.background("#fff");
-    Crafty.e("2D, DOM, Text").attr({ w: 100, h: 20, x: 150, y: 120 })
-            .text("Loading")
-            .css({ "text-align": "center", "color": "#000" });		
-	
-});//end scene
